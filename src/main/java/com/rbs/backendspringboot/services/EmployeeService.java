efcdfb8421aa86ac04717058c831a7e4ea67b0d8
Ahmed Al-Salih <ahmed.al-salih@delhaize.com>
2020-02-11 15:59:51 -0500

    update serevice anotation

--- src/main/java/com/rbs/backendspringboot/services/EmployeeService.java
+++ src/main/java/com/rbs/backendspringboot/services/EmployeeService.java
@@ -4,21 +4,22 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import com.rbs.backendspringboot.model.Employee;
 
-@Component
+@Service
 public class EmployeeService {
 	
 	
 	private List<Employee> employees = new ArrayList <> ( 
 			Arrays.asList(
+		new Employee(5,"Ahmed","ALSALIH","Mooresvile","123456789"),
 		new Employee(1,"Mae","Forbes","Charlotte","123456789"),
-		new Employee(2,"Sean","Fallmann","Charlotte","123456789"),
-		new Employee(3,"Aaron","Broom","Charlotte","123456789"),
-		new Employee(4,"Brian","Lindner","Charlotte","123456789"),
-		new Employee(5,"Ahmed","ALSALIH","Charlotte","123456789")
+		new Employee(2,"Sean","Fallmann","somwhere","123456789"),
+		new Employee(3,"Aaron","Broom","Concord","123456789"),
+		new Employee(4,"Brian","Lindner","Mooresvile","123456789")
+		
 			));
 	
 	public List<Employee> getAllEmployees(){
