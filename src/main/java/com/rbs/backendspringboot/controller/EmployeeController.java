9ebc02ad614b1b5e3ac9930474339f53a6ae372d
Ahmed Al Salih <a.alsalih2@gmail.com>
2020-02-14 00:15:35 -0500

    return the location of the new created record in the header as URL for the client to use for Rest call

--- src/main/java/com/rbs/backendspringboot/controller/EmployeeController.java
+++ src/main/java/com/rbs/backendspringboot/controller/EmployeeController.java
@@ -3,7 +3,10 @@ package com.rbs.backendspringboot.controller;
 import java.util.List;
 import java.util.Optional;
 
+import javax.xml.ws.spi.http.HttpHandler;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +16,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.rbs.backendspringboot.model.Employee;
 import com.rbs.backendspringboot.services.EmployeeService;
@@ -44,17 +48,30 @@ public class EmployeeController {
 	public ResponseEntity<Employee> getEmployeeById2(@RequestParam("id") int id) {
 		
 		Employee e = employeeservice.getEmployeeById(id);
+		HttpHeaders httpHeaders = new HttpHeaders();
+		httpHeaders.add("Ahmed-key", "RBS-Value-1");
+		httpHeaders.add("Sean-key", "RBS-Value-2");
+		httpHeaders.add("Brian-key", "RBS-Value-3");
 		if (e != null ){
-			return new ResponseEntity<Employee>(e, HttpStatus.OK);
+			return new ResponseEntity<Employee>(e,httpHeaders, HttpStatus.OK);
 		}else
-		return new ResponseEntity("Not found",HttpStatus.NOT_FOUND);
+		return new ResponseEntity("Not found", httpHeaders, HttpStatus.NOT_FOUND);
 		
 	}
 	
 
 	@PostMapping("/employees")
-	public Employee AddEmployee(@RequestBody Employee emp){
-		return employeeservice.addemployee(emp);
+	public ResponseEntity<Employee> AddEmployee(@RequestBody Employee emp){
+		employeeservice.addemployee(emp);
+		HttpHeaders httpHeaders = new HttpHeaders();
+		httpHeaders.add(
+						"location",
+						ServletUriComponentsBuilder.fromCurrentRequest()
+						.path("/{id}")
+						.buildAndExpand(emp.getId()).toUri()
+						.toString());
+		
+		return new ResponseEntity <Employee>(emp,httpHeaders,HttpStatus.CREATED);
 	}
 
 }
