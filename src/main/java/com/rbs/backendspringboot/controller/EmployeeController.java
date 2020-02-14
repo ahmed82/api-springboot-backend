d25dcef841a56f612ca6046cd5a74f440c641079
Sean Fallmann <1644733@delhaize.com>
2020-02-14 11:25:19 -0500

    Added Update and Delete routes

--- src/main/java/com/rbs/backendspringboot/controller/EmployeeController.java
+++ src/main/java/com/rbs/backendspringboot/controller/EmployeeController.java
@@ -1,17 +1,19 @@
 package com.rbs.backendspringboot.controller;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rbs.backendspringboot.model.Employee;
@@ -54,7 +56,19 @@ public class EmployeeController {
 
 	@PostMapping("/employees")
 	public Employee AddEmployee(@RequestBody Employee emp){
-		return employeeservice.addemployee(emp);
+		return employeeservice.upsertEmployee(emp);
 	}
+	
 
+	@PatchMapping("/employees/{id}")
+	public Employee updateEmployee(@RequestBody Employee emp, @PathVariable int id) {
+		emp.setId(id);
+		return employeeservice.upsertEmployee(emp);
+	}	
+
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	@DeleteMapping("/employees")
+	public @ResponseBody void deleteEmployees(@RequestBody List<Employee> emps) {
+		employeeservice.deleteEmployee(emps);
+	}
 }
