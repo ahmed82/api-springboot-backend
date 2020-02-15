07f4b885b88ee6453c847ccd43d543416b288d3c
Ahmed Al Salih <a.alsalih2@gmail.com>
2020-02-15 11:35:54 -0500

    create error.html page

--- src/main/java/com/rbs/backendspringboot/exception/GlobalExceptionHundelerMethod.java
+++ src/main/java/com/rbs/backendspringboot/exception/GlobalExceptionHundelerMethod.java
@@ -1,8 +1,20 @@
 package com.rbs.backendspringboot.exception;
 
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
 
 @ControllerAdvice
 public class GlobalExceptionHundelerMethod {
+	
+	@ExceptionHandler(NullPointerException.class)
+	public ModelAndView handleException(NullPointerException ex)
+	{
+	    //Do something additional if required
+	    ModelAndView modelAndView = new ModelAndView();
+	    modelAndView.setViewName("error");
+	    modelAndView.addObject("message", ex.getMessage());
+	    return modelAndView;
+	}
 
 }
