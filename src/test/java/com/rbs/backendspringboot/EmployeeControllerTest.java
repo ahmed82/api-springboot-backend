aad1add2873d09377c123e820a0be5a9655e1dff
Ahmed Al Salih <a.alsalih2@gmail.com>
2020-02-20 18:50:45 -0500

    convert to regretion test

--- src/test/java/com/rbs/backendspringboot/EmployeeControllerTest.java
+++ src/test/java/com/rbs/backendspringboot/EmployeeControllerTest.java
@@ -5,7 +5,12 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
 class EmployeeControllerTest {
 
 	@BeforeEach
