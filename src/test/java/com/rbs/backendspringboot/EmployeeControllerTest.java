3da6c0f0b0abf4685be5ee6b57e895fc7adb4c95
Ahmed Al Salih <a.alsalih2@gmail.com>
2020-02-20 08:10:20 -0500

    create Junit 5 test

---
+++ src/test/java/com/rbs/backendspringboot/EmployeeControllerTest.java
@@ -0,0 +1,46 @@
+package com.rbs.backendspringboot;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+class EmployeeControllerTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	@Disabled // @Ignore used for previous version of Junit
+	void testGetAllEmployee() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetEmployeeById() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetEmployeeById2() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAddEmployee() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testUpdateEmployee() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDeleteEmployees() {
+		fail("Not yet implemented");
+	}
+
+}
